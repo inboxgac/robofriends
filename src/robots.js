2c395bb5658dbcc0e741c6e49857e60a4c5896af
Andrzej Gacek <inboxgac@gmail.com>
2019-09-21 17:58:35 +0200

    first commit

---
+++ src/robots.js
@@ -0,0 +1,30 @@
+export const robots = [
+  {
+    id: 1,
+    name: 'Leanne Graham',
+    username: 'Bret',
+    email: 'Sencere@april.biz'
+  },
+  {
+    id: 2,
+    name: 'Clementine Howell',
+    username: 'Ervin',
+    email: 'Clementine@google.com'
+
+  },
+  {  id: 3,
+    name: 'Clementine Bauch',
+    username: 'Bauch',
+    email: 'Bauch@msn.com'
+  },
+  {  id: 4,
+    name: 'Clementine Jon',
+    username: 'Jon',
+    email: 'Jon@msn.com'
+  },
+  {  id: 5,
+    name: 'Leanne Graham',
+    username: 'Jacek',
+    email: 'Jacek@msn.com'
+  }
+]
